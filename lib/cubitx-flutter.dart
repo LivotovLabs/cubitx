7cd86d0b5d9f0698e23a9bee594dffc0ed981e11
Dr Watson <>
2023-03-23 17:12:31 +0300

    initial project and example inject

---
+++ lib/cubitx-flutter.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'cubitx-logic.dart';
+
+abstract class CubitView<C extends Cubit<S>, S> extends StatefulWidget {
+  final C controller;
+
+  const CubitView(this.controller, {super.key});
+
+  @override
+  State createState() => _CubitViewState(controller);
+
+  Widget _build(BuildContext context) {
+    return onComposeWidget(context, controller.state());
+  }
+
+  Widget onComposeWidget(BuildContext context, S state);
+}
+
+class _CubitViewState<C extends Cubit> extends State<CubitView> {
+  final Cubit controller;
+  final _observer = RxNotifier();
+  late StreamSubscription subs;
+
+  _CubitViewState(this.controller) : super();
+
+  @override
+  void initState() {
+    super.initState();
+    subs = _observer.listen(_updateTree, cancelOnError: false);
+  }
+
+  void _updateTree(_) {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.stopCubit();
+    subs.cancel();
+    _observer.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => RxInterface.notifyChildren(_observer, () => widget._build(context));
+}
---
+++ lib/cubitx-logic.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:get/get.dart';
+
+abstract class Cubit<S> {
+  late Rx<S> _state;
+  final List<StreamSubscription> _subs = [];
+
+  S state() => _state.value;
+
+  Cubit(S initialState, {List<Cubit> dependencies = const <Cubit>[]}) {
+    _state = Rx(onStateComputed(initialState));
+
+    for (var dep in dependencies) {
+      _subs.add(dep._state.listen((ds) =>
+      {
+        _state.value = onStateComputed(_state.value)
+      }));
+    }
+
+    onStarted();
+  }
+
+  void stopCubit() {
+    for (var sub in _subs) {
+      sub.cancel();
+    }
+    _subs.clear();
+    onStopped();
+  }
+
+  void onStarted() {}
+
+  void onStopped() {}
+
+  void update(S state) {
+    _state.value = onStateComputed(state);
+  }
+
+  S onStateComputed(S state) {
+    return state;
+  }
+}
